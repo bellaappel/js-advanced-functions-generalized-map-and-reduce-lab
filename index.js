53d3afdbfc0d02719aed22de84f0c6a84f2c435e
bellaappel <bellaappel0@gmail.com>
2021-04-01 11:50:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(srcArray, func){
+    let newArray = []
+    for(let i = 0; i < srcArray.length; i++){
+    newArray.push(func(srcArray[i]))
+    }
+    return newArray
+};
+
+function reduce(srcArray, func, start=0){
+     let total = (!!start) ? start : srcArray[0]
+    let i = (!!start) ? 0 : 1
+    for(; i < srcArray.length; i++){
+        total = func(srcArray[i], total)
+    }
+    return total
+}
